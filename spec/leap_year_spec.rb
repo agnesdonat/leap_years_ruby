cab9ef5d05a3692c069c0ba57545dac62e4e929d
agnesdonat <agi.donat@gmail.com>
2018-05-25 16:43:30 +0100

    Tests for year divisible by 4

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -9,4 +9,12 @@ describe '#calculator' do
   it 'checks if the year is divisible by 100 but not by 400 ' do
     expect(calculator(2100)).to eq "It's a common year."
   end
+
+  it 'checks if the year is divisible by 4 but not by 100' do
+    expect(calculator(2008)).to eq "It's a leap year."
+  end
+
+  it 'checks if the year is not divisible by 4' do
+    expect(calculator(2017)).to eq "It's a common year."
+  end
 end
