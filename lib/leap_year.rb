82461a3670afe527c0e9e9d7d0b2c52867e80934
agnesdonat <agi.donat@gmail.com>
2018-05-25 16:15:08 +0100

    Test for common year part one

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,5 +1,7 @@
 def calculator(year)
-  if year === 2020
+  if year % 4 != 0
+    return "It's a common year."
+  else
     return "It's a leap year."
   end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -2,7 +2,11 @@
 require 'leap_year'
 
 describe '#calculator' do
-  it 'checks if year is leap year or common year' do
+  it 'checks if the year is a leap year' do
     expect(calculator(2020)).to eq "It's a leap year."
+  end
+
+  it 'checks if the year is a common year ' do
+    expect(calculator(2018)).to eq "It's a common year."
   end
 end
