3062773e58f2097e0689f4dea4e57d51fab9bb33
agnesdonat <agi.donat@gmail.com>
2018-05-29 16:33:05 +0100

    Added calculator method to LeapYear class

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,5 +1,10 @@
-def calculator(year)
-  raise "Please enter a valid year!" unless (year.is_a? Integer) && (year > 0)
-  return "It's a common year." if year % 4 != 0 || year % 400 != 0 && year % 100 == 0
-  "It's a leap year."
+
+class LeapYear
+
+  def calculator(year)
+    raise "Please enter a valid year!" unless (year.is_a? Integer) && (year > 0)
+    return "It's a common year." if year % 4 != 0 || year % 400 != 0 && year % 100 == 0
+    "It's a leap year."
+  end
+
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,30 +1,32 @@
 # Write an app that will take a number and tell you if it is a leap year.
 require 'leap_year'
 
-describe '#calculator' do
+ describe LeapYear do
+ 
+  describe '#calculator' do
+    it 'raises an error if user input is not an integer' do
+      expect { subject.calculator("ducks") }.to raise_error("Please enter a valid year!")
+    end
 
-  it 'raises an error if user input is not an integer' do
-    expect { calculator("ducks") }.to raise_error("Please enter a valid year!")
+    it 'raises an error if user input is negative' do
+      expect { subject.calculator(-1988) }.to raise_error("Please enter a valid year!")
+    end
+
+    it 'checks if the year is divisible 400' do
+      expect(subject.calculator(2000)).to eq "It's a leap year."
+    end
+
+    it 'checks if the year is divisible by 100 but not by 400 ' do
+      expect(subject.calculator(2100)).to eq "It's a common year."
+    end
+
+    it 'checks if the year is divisible by 4 but not by 100' do
+      expect(subject.calculator(2008)).to eq "It's a leap year."
+    end
+
+    it 'checks if the year is not divisible by 4' do
+      expect(subject.calculator(2017)).to eq "It's a common year."
+    end
   end
 
-  it 'raises an error if user input is negative' do
-    expect { calculator(-1988) }.to raise_error("Please enter a valid year!")
-  end
-
-
-  it 'checks if the year is divisible 400' do
-    expect(calculator(2000)).to eq "It's a leap year."
-  end
-
-  it 'checks if the year is divisible by 100 but not by 400 ' do
-    expect(calculator(2100)).to eq "It's a common year."
-  end
-
-  it 'checks if the year is divisible by 4 but not by 100' do
-    expect(calculator(2008)).to eq "It's a leap year."
-  end
-
-  it 'checks if the year is not divisible by 4' do
-    expect(calculator(2017)).to eq "It's a common year."
-  end
 end
